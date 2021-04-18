e149b2c350ff95076ff0fcbfe693caf0f9a4685b
guplem <guplem@gmail.com>
2021-04-18 11:26:16 +0200

    Sound Manager v1

---
+++ TS21-Cheering/Assets/Scripts/AppManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    public static AppManager Instance { get; private set; }
+    [SerializeField] SoundManager soundManager;
+    
+    private void Awake()
+    {
+        AssignSingletonInstance();
+    }
+    
+    private void AssignSingletonInstance()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Debug.LogError("More than one AppManager exist", this);
+    }
+    
+    
+}
---
+++ TS21-Cheering/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private int defaultAudioSourcesQty;
+    [HideInInspector] private List<AudioSource> audioSources;
+
+    private void Awake()
+    {
+        audioSources = new List<AudioSource>();
+
+        foreach (AudioSource audioSource in GetComponents<AudioSource>())
+            audioSources.Add(audioSource);
+
+        for (int i = audioSources.Count; i < defaultAudioSourcesQty; i++)
+            AddAudioSource();
+    }
+
+
+    //ToDo: Check if the limit of the pitch and pitchRandomization is -3 to 3
+    public void PlaySound(AudioClip clip, float volume, bool loop, float pitch, float pitchRandomization, AudioMixerGroup audioMixerGroup)
+    {
+        if (clip == null)
+        {
+            Debug.LogWarning("Trying to play a null clip", this);
+            return;
+        }
+
+        ConfigureAudioSource(GetFreeAudioSource(), clip, volume, loop, pitch, pitchRandomization, audioMixerGroup).Play();
+    }
+
+    private AudioSource GetFreeAudioSource()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (!audioSource.isPlaying)
+            {
+                return audioSource;
+            }
+        }
+
+        return AddAudioSource();
+    }
+
+    // ToDo: check if this is the way is wanted to be the default configuration
+    private AudioSource AddAudioSource()
+    {
+        AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
+        newAudioSource.playOnAwake = false;
+        newAudioSource.spatialBlend = 1f;
+        newAudioSource.rolloffMode = AudioRolloffMode.Linear;
+        newAudioSource.minDistance = 0.0f;
+        newAudioSource.maxDistance = 30.0f; 
+        audioSources.Add(newAudioSource);
+
+        return newAudioSource;
+    }
+
+    private AudioSource ConfigureAudioSource(AudioSource audioSource, AudioClip clip, float volume, bool loop, float pitch, float pitchRandomization, AudioMixerGroup audioMixerGroup)
+    {
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.outputAudioMixerGroup = audioMixerGroup;
+        audioSource.pitch = pitchRandomization;
+
+        audioSource.loop = loop;
+
+        return audioSource;
+    }
+
+    public void StopAllSounds(bool fadeOut = false)
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (fadeOut)
+            {
+                //ToDo: Be able to stop the sounds with any type of animation curve
+                FadeOutAudioSource(audioSource);
+            }
+            else
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+    private void FadeOutAudioSource(AudioSource audioSource)
+    {
+        StartCoroutine(LowerVolumeAndStopSounds(audioSource));
+    }
+
+    private static IEnumerator LowerVolumeAndStopSounds(AudioSource audioSource)
+    {
+        while (audioSource.volume > 0f)
+        {
+            audioSource.volume -= 0.1f;
+            yield return new WaitForSeconds(0.12f);
+
+        }
+        audioSource.Stop();
+        audioSource.volume = 1.0f;
+    }
+    
+}
